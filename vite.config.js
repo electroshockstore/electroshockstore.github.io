017c2321dd227a5eb1eb2a039c109c0a50591c30
Lemos <jonathan.lemos@aubasa.com.ar>
2025-11-30 19:00:18 -0300

    hero view

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/catalogo-venta/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
