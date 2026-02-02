78a4dbd0d0c75624cba31d4f7bfda183cb73e62b
Lemos <jonathan.lemos@aubasa.com.ar>
2026-02-02 12:13:43 -0300

    Updates

--- assets/vendor-DkGxx3Y7.js
+++ assets/vendor-DkGxx3Y7.js
@@ -1,4 +1,4 @@
-var uo=Math.pow;import{y as Er,z as H}from"./react-core-Bf60W6Tt.js";var pt={exports:{}},vt={};/**
+var uo=Math.pow;import{y as Er,z as H}from"./react-core-mzQlS9Ye.js";var pt={exports:{}},vt={};/**
  * @license React
  * scheduler.production.min.js
  *
