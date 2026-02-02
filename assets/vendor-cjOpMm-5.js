84c991d46f1d51ded4b2a5046ec11329826dee38
Lemos <jonathan.lemos@aubasa.com.ar>
2026-02-02 11:47:00 -0300

    Updates

--- assets/vendor-cjOpMm-5.js
+++ assets/vendor-cjOpMm-5.js
@@ -1,4 +1,4 @@
-var uo=Math.pow;import{y as Er,z as H}from"./react-core-YQes1nAs.js";var pt={exports:{}},vt={};/**
+var uo=Math.pow;import{y as Er,z as H}from"./react-core-Bf60W6Tt.js";var pt={exports:{}},vt={};/**
  * @license React
  * scheduler.production.min.js
  *
